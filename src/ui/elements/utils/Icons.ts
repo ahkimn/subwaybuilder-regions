da91c2b7e0d101fd71055dcbb042377ef6947fc2
Alex Kimn <alex.kimn@outlook.com>
2026-02-22 12:37:25 +0900

    new icons

--- src/ui/elements/utils/Icons.ts
+++ src/ui/elements/utils/Icons.ts
@@ -3,13 +3,13 @@ import type { createElement, ReactNode } from 'react';
 type SVGNode =
   | { tag: 'path'; d: string; strokeWidth?: string; strokeLinecap?: string }
   | {
-      tag: 'rect';
-      x: number;
-      y: number;
-      width: number;
-      height: number;
-      rx?: number;
-    }
+    tag: 'rect';
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    rx?: number;
+  }
   | { tag: 'circle'; cx: number; cy: number; r: number };
 
 export interface IconDefinition {
@@ -196,3 +196,25 @@ export const MapPinnedIcon: IconDefinition = {
     },
   ],
 };
+
+export const TriangleWarning: IconDefinition = {
+  nodes: [
+    {
+      tag: 'path',
+      d: 'm21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3',
+    },
+    { tag: 'path', d: 'M12 9v4' },
+    { tag: 'path', d: 'M12 17h.01' },
+  ],
+}
+
+export const OctagonX: IconDefinition = {
+  nodes: [
+    {
+      tag: 'path',
+      d: 'M2.586 16.726A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2h6.624a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586z',
+    },
+    { tag: 'path', d: 'm9 9 6 6' },
+    { tag: 'path', d: 'm15 9-6 6' },
+  ],
+}
